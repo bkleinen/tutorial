c2bd30f4eacf89748914b6b61bbaacd012a51414
Bela Kleinen <kleinen@infrastructure.de>
2011-03-16 12:32:26 +0100

    finished chapter 7

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,8 @@ describe User do
       :password => "foobar",
       :password_confirmation => "foobar"
     }
+  # for copying into consoles:
+  # a = {      :name => "Example User",      :email => "user@example.com",    :password => "foobar",    :password_confirmation => "foobar"   }
   end
   it "should create a new instance given valid attributes" do
     user = User.create!(@attr)
