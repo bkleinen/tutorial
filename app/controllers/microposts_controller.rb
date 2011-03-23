c6eaee72a259eb22af5674366863a489367ae6f8
Bela Kleinen <kleinen@infrastructure.de>
2011-03-23 21:50:36 +0100

    chapter 11 finished

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -7,10 +7,21 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost created!"
       redirect_to root_path
     else
+      @feed_items = []
       render 'pages/home'
     end
   end
 
-  def destroy
+ def destroy
+    @micropost.destroy
+    redirect_back_or root_path
   end
+
+  private
+
+    def authorized_user
+      @micropost = Micropost.find(params[:id])
+      redirect_to root_path unless current_user?(@micropost.user)
+    end
+
 end
