998e50607b613e5daa1c3a2ec949504ad29e534a
Barbara Kleinen <kleinen@infrastructure.de>
2011-03-24 01:58:26 +0100

    janus

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,7 +38,7 @@ class User < ActiveRecord::Base
    has_many :reverse_relationships, :foreign_key => "followed_id", 
                                     :class_name => "Relationship",
                                     :dependent => :destroy
-   has_many :followers, :through => :reverse_relationships, source => :follower
+   has_many :followers, :through => :reverse_relationships, :source => :follower
   
   validates :name, :presence => true , :length => { :maximum => 50 } 
   
